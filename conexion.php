7386686e4c332f9ddbd3197686f13b4dd07f3c0e
cesarlurin <cesarmarca@iestplurin.edu.pe>
2026-07-19 22:55:01 -0500

    cambioscesar

--- conexion.php
+++ conexion.php
@@ -3,8 +3,10 @@ $host = "localhost";
 $user = "root";
 $pass = "";
 $db   = "sistema-peliculas"; // Nombre actualizado
+$port = 3307; // Puerto donde corre tu MySQL de XAMPP
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db ,$port);
+
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
